4673e53dbe5ff5a6251b3d848e9675d514b527e1
Bastian <106901362+wiesty@users.noreply.github.com>
2023-02-21 20:40:47 +0100

    Create script.bat

---
+++ script.bat
@@ -0,0 +1,44 @@
+@echo off
+title Backup
+
+REM Define backup source and destination folders
+set backupSource=C:\Users\wiesty\BackupSource
+set tempbackup=C:\Users\wiesty\temp
+set backupDestination=C:\Users\wiesty\FinalBackups
+
+
+:check_connection
+echo checking internet connection....
+ping 8.8.8.8 -n 1 -w 1000 >nul
+if errorlevel 1 (
+    echo You don't have an internet connection. Waiting 10 seconds before retrying...
+    timeout /t 10 /nobreak >nul
+    goto check_connection
+)
+
+
+echo creating new backup folder...
+set backupFolder=%date:~6,4%-%date:~3,2%-%date:~0,2%_%time:~0,2%-%time:~3,2%
+set backupFolder=%backupFolder:_=-%
+mkdir "%tempbackup%\%backupFolder%"
+
+echo folder created. backup starting...
+REM ausgeschlossene Ordner kommen nach /np /xd mit ""
+robocopy "%backupSource%" "%tempbackup%%backupFolder%" /e /xo /r:0 /np /xd "%backupSource%\yt" "%backupSource%\magicmirror2" "%backupSource%\homematic" "%backupSource%\homematic-wiesty" "%backupSource%\magicmirror\modules" "%backupSource%\portainer-ce" "%backupSource%\NginxProxyManager\logs" "%backupSource%\NginxProxyManager\log" "%backupSource%\mysql"
+echo Backup complete.
+echo.
+echo Zip the backup folder
+powershell Compress-Archive -Path "%tempbackup%%backupFolder%" -DestinationPath "%backupDestination%\%backupFolder%.zip"
+echo done
+echo.
+echo Delete the original backup folder
+rmdir /s /q "%tempbackup%\%backupFolder%"
+echo done
+echo.
+echo deleting old backups...
+pushd "%backupDestination%"
+for /f "skip=10 delims=" %%f in ('dir /b /o-d /a-d *.zip') do del "%%f"
+popd
+echo Old backups deleted.
+echo.
+pause
